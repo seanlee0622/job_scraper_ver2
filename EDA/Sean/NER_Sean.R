a4b2b6aa02522945fb3ed5f69f1bc229dcdf48cc
Sean Lee <49972734+seanlee0622@users.noreply.github.com>
2022-09-30 21:13:10 -0600

    test

---
+++ EDA/Sean/NER_Sean.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(digest)
+library(dplyr)
+library(httr)
+
+
+df <- httr::GET("https://rstudio-connect.business.utah.edu/content/260/data") %>% 
+  httr::content()
+df
+
+
